b839239fb147804a105603dfd61211c0e3e0af24
chilejiang1024 <jiangchile@JiangChile.local>
2016-01-22 11:40:49 +0800

    create window

--- MusicPlayerEasy/MusicPlayerEasy/AppDelegate.m
+++ MusicPlayerEasy/MusicPlayerEasy/AppDelegate.m
@@ -18,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    ViewController *root = [[ViewController alloc] init];
+    self.window.rootViewController = root;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
